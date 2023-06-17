f2229af2d740794a784adf7f3f4d6bbf50ccf569
Duman Haydar <haydar@duman.io>
2023-06-17 11:19:32 +0200

    add gpio wath button

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,25 @@ import express from 'express'
 import path from 'path'
 import http from 'http'
 import { Server } from 'socket.io'
+import { Gpio } from 'onoff'
 const app = express()
 const server = http.createServer(app)
 const port = 3000
 const io = new Server(server)
+const button = new Gpio(4, 'in', 'both')
 
 app.use(express.static(path.resolve(__dirname, './client')))
 
 io.on('connection', (socket) => {
   console.log('a user connected')
+})
+
+button.watch((err, value) => {
+  if (err != null) {
+    throw err
+  }
+
+  io.emit('chat message', 'test')
 })
 
 app.get('*', (req, res) => {
@@ -19,4 +29,8 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Express is listening at http://localhost:${port}`)
+})
+
+process.on('SIGINT', _ => {
+  button.unexport()
 })
