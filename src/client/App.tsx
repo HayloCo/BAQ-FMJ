a68082fe7ad9afbfd61322c07293a09703e8258c
Duman Haydar <haydar@duman.io>
2023-06-24 14:35:09 +0200

    update

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -61,7 +61,7 @@ const App: FC = () => {
           setTimeout(() => {
             setPlaying('ready');
           }, config.delay * 1000);
-      } else if (playing === 'on_play' && index < (config.slideSize ? config.slideSize : slides.length)) {
+      } else if (playing === 'on_play' && index + 1 < (config.slideSize ? config.slideSize : slides.length)) {
         setIndex((prevIndex) => {
           const newIndex = (prevIndex + 1) % (config.random ? randomNumbers.length : slides.length);
           if(config.random) {
